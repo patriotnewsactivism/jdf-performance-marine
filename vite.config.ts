9df348b6c82273db4e2f57823767b39beb7b4f75
Matthew Reardon <patriotnewsactivism@proton.me>
2025-11-01 03:34:29 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { Analytics } from "@vercel/analytics/next";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
