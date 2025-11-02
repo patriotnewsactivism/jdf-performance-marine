1cb8c17829cae9f844814a185b2666636e70e01f
Matthew Reardon <patriotnewsactivism@proton.me>
2025-11-02 14:27:41 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import ChatBot from "./components/ChatBot";
 import { Analytics } from "@vercel/analytics/next";
+import ScrollToTop from "./components/ScrollToTop";
 
 const queryClient = new QueryClient();
 
@@ -21,6 +22,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop />
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
